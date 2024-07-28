68981079409009608348bfd6d6ee6b3f8affa075
aryanoverdose <panditaryan.as@gmail.com>
2024-07-28 18:14:53 +0530

    simple api created

--- week2/index.js
+++ week2/index.js
@@ -1,19 +1,59 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const sum = (n) => {
-    let num = 0
-    for (let i = 0; i <= n; i++) {
-        num = num + i
+const users = [{
+    name: 'john',
+    kidneys: [
+        {
+            healthy: false,
+        }
+    ]
+}]
+
+app.use(express.json());
+
+app.get('/', function (req, res) {
+    const userKidneys = users[0].kidneys;
+    const numberOfKidneys = userKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i < userKidneys.length; i++) {
+        if (userKidneys[i].healthy) {
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
+
     }
-    return num
-}
-
-app.get('/', (req, res) => {
-    const n = req.query.n
-    const ans = sum(n)
-    res.send('your answer is ' + ans)
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
 })
 
 
-app.listen(3000)
+
+app.post('/', function (req, res) {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({ healthy: isHealthy })
+    res.json({ msg: 'Done!' })
+})
+
+app.put('/', function (req, res) {
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true
+    }
+    res.json({})
+})
+
+app.delete('/', function (req, res) {
+    const newKidneys = []
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        if (users[0].kidneys[i].healthy) {
+            newKidneys.push({ healthy: true })
+        }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({ msg: 'Done!' })
+})
+
+app.listen(3000);
