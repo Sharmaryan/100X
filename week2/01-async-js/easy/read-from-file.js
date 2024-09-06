c9adb53b15b7ef1ed2b618a9c5a61c35bece21b6
aryanoverdose <panditaryan.as@gmail.com>
2024-09-06 21:15:51 +0530

    refactor : added expensive operation

--- week2/01-async-js/easy/read-from-file.js
+++ week2/01-async-js/easy/read-from-file.js
@@ -3,3 +3,11 @@ const fs = require('fs');
 fs.readFile('file/test.txt','utf-8',(_, data) => {
     console.log(data)
 })
+
+
+// expensive operation
+let a = 0
+for (let i = 0; i < 999999999; i++) {
+  a= a+i  
+}
+console.log(a)
