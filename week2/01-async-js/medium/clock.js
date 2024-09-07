47a7bc9e95b5872664a27ea1e497e2c4c2ec258c
aryanoverdose <panditaryan.as@gmail.com>
2024-09-07 11:05:16 +0530

    feat : added am/pm in clock

--- week2/01-async-js/medium/clock.js
+++ week2/01-async-js/medium/clock.js
@@ -1,5 +1,18 @@
+// setInterval(() => {
+//     const date = new Date()
+//     const formattedTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+//     console.log(formattedTime)
+// }, 1000)
+
+// with am/pm
+const NINE = 9
 setInterval(() => {
     const date = new Date()
-    const formattedTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    console.log(formattedTime)
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+    const getTimeWithClockSystem = (clockSystem) => {
+        return `${hour}:${minute <= NINE ? '0' + minute : minute}:${second <= NINE ? '0' + second : second} ${clockSystem}`
+    }
+    console.log(hour >= 12 && minute >= 0 ? getTimeWithClockSystem('pm') : getTimeWithClockSystem('am'))
 }, 1000)
