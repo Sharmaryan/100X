2c59fbf46e71ef5ace21eb4228220b7795a2ba99
aryanoverdose <panditaryan.as@gmail.com>
2024-09-09 20:29:35 +0530

    written schema using zod

--- week3/index1.js
+++ week3/index1.js
@@ -1,16 +1,22 @@
 const express = require('express')
+const zod = require('zod')
 const app = express()
+const schema = zod.array(zod.number())
 app.use(express.json())
 
 app.post('/health-checkup', (req, res) => {
     const kidneys = req.body.kidneys
-    const kidneyLength = kidneys.length
-    res.send('you have ' + kidneyLength + ' kidneys')
-
+    const response = schema.safeParse(kidneys)
+    if (!response.success) {
+        res.status(411).json({ msg: 'Invalid inputs' })
+    }
+    else {
+        res.send({ response })
+    }
 })
 
 app.use((err, req, res, next) => {
-    res.json({msg:'something went wrong'})
+    res.json({ msg: 'something went wrong' })
 })
 
 app.listen(3000)
