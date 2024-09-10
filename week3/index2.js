9d39ee63ef0599308428bae3e3053d4f7c135808
aryanoverdose <panditaryan.as@gmail.com>
2024-09-10 20:56:42 +0530

    feat : connected to db

--- week3/index2.js
+++ week3/index2.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const jwtPassword = "Test@123";
+
+mongoose.connect(
+    'mongodb+srv://Sharmaryan:Aryan8630@nodeexpressprojects.02bwfcu.mongodb.net/',
+);
+const User = mongoose.model('Users', { name: String, email: String, password: String });
+
 
 const app = express();
 app.use(express.json())
 
-const ALL_USERS = [
-    {
-        username: "harkirat@gmail.com",
-        password: "123",
-        name: "harkirat singh",
-    },
-    {
-        username: "aryan@gmail.com",
-        password: "Test@123",
-        name: "Aryan Sharma",
-    },
-    {
-        username: "priya@gmail.com",
-        password: "123321",
-        name: "Priya kumari",
-    },
-];
-
 function userExists(username, password) {
-    return ALL_USERS.find((user) => user.username === username && user.password === password)
+    // return ALL_USERS.find((user) => user.username === username && user.password === password)
 }
+
+app.post("/signup", async function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+    const existingUser = await User.findOne({ email: username })
+    if (existingUser) {
+        return res.status(400).send("User already exists")
+    }
+    const user = new User({ email: username, password, name })
+    user.save()
+    res.json(({ "msg": "User created successfully" }))
+});
+
+
 
 app.post("/signin", function (req, res) {
     const username = req.body.username;
@@ -51,7 +55,6 @@ app.get("/users", function (req, res) {
         const otherUsers = ALL_USERS.filter((user) => user.username !== username);
         res.status(200).send(otherUsers)
         return otherUsers
-        // return a list of users other than this username
     } catch (err) {
         return res.status(403).json({
             msg: "Invalid token",
