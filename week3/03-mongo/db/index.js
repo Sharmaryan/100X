6881cb2c2f68f5e067f0181f9946fb3f7d9c5a5d
aryanoverdose <panditaryan.as@gmail.com>
2024-09-14 19:26:33 +0530

    feat : created schema

--- week3/03-mongo/db/index.js
+++ week3/03-mongo/db/index.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://Sharmaryan:Aryan8630@nodeexpressprojects.02bwfcu.mongodb.net/');
+
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imageLink: String,
+    published: Boolean,
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
