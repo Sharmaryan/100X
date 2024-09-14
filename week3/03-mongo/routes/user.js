4ef64e579e64e3a70bf95f0530b53d3f54f367b0
aryanoverdose <panditaryan.as@gmail.com>
2024-09-14 21:41:35 +0530

    feat : signup route

--- week3/03-mongo/routes/user.js
+++ week3/03-mongo/routes/user.js
@@ -1,9 +1,20 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../db");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const payload = { username, password }
+    if (username && password) {
+        const admin = await User(payload)
+        if (admin?.id) {
+            admin.save()
+            res.json({ message: 'User created successfully' })
+        }
+    }
     // Implement user signup logic
 });
 
