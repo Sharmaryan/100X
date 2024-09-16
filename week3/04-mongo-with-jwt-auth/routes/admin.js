63616878400143c4ca7538f45eb9e795526e7e80
aryanoverdose <panditaryan.as@gmail.com>
2024-09-16 21:13:39 +0530

    feat : create admin routes

--- week3/04-mongo-with-jwt-auth/routes/admin.js
+++ week3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,22 +1,63 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
+const jwt = require('jsonwebtoken');
+// will remove later
+const SECRET_KEY = 'hey_secret'
 
 // Admin Routes
-router.post('/signup', (req, res) => {
-    // Implement admin signup logic
+router.post('/signup', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const payload = { username, password }
+    try {
+        await Admin.create(payload)
+        res.json({ msg: 'Admin Created Successfully' })
+    }
+    catch (err) {
+        res.json({ msg: 'Unable to create admin' })
+    }
 });
 
-router.post('/signin', (req, res) => {
-    // Implement admin signup logic
+router.post('/signin', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+    const payload = { username, password }
+    try {
+        const user = await Admin.findOne(payload)
+        if (user?.id) {
+            const token = jwt.sign({ username }, SECRET_KEY)
+            res.json({ token })
+        }
+        else
+            throw err
+    }
+    catch (err) {
+        res.json({ msg: 'user not found' })
+    }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
-    // Implement course creation logic
+router.post('/courses', adminMiddleware, async (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
+    const price = req.body.price
+    const imageLink = req.body.imageLink
+    const payload = { title, description, price, imageLink, published: true }
+    if (title && description && price && imageLink) {
+        const course = await Course.create(payload)
+        if (course?.id) {
+            res.send({ message: 'Course created successfully', courseId: course.id })
+        }
+    }
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
-    // Implement fetching all courses logic
+router.get('/courses', adminMiddleware, async (_, res) => {
+    const listOfCourses = await Course.find({})
+    if (!!listOfCourses.length) {
+        res.send(listOfCourses)
+    }
 });
 
 module.exports = router;
