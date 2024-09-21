7c5a129f562f604819ee0057a26a3114a966331b
aryanoverdose <panditaryan.as@gmail.com>
2024-09-21 17:42:21 +0530

    created counter using recoil

--- week7/context/src/App.jsx
+++ week7/context/src/App.jsx
@@ -1,57 +1,123 @@
 import React, { Suspense } from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
-const Dashboard = React.lazy(() => import("./components/Dashboard"));
-const Landing = React.lazy(() => import("./components/Landing"));
+// import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
+import { countAtom, evenSelector } from "./store/atoms/count";
+// const Dashboard = React.lazy(() => import("./components/Dashboard"));
+// const Landing = React.lazy(() => import("./components/Landing"));
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <AppBar />
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={
-              <Suspense fallback={<h1>loading...</h1>}>
-                <Dashboard />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <Suspense fallback={<h1>loading...</h1>}>
-                <Landing />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
     </div>
   );
 }
 
-function AppBar() {
-  const navigate = useNavigate();
+function Count() {
+  console.log("re-render");
+  return (
+    <div>
+
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  console.log(count)
+
+  return (
+    <div>
+      <b>{count}</b>
+      <EvenCountRenderer />
+    </div>
+  );
+}
+
+function EvenCountRenderer() {
+  const isEven = useRecoilValue(evenSelector);
+
+  return <div>{isEven ? "It is even" : null}</div>;
+}
+
+function Buttons() {
+  const setCount = useSetRecoilState(countAtom);
+  console.log("buttons re-rendererd");
+
   return (
     <div>
       <button
         onClick={() => {
-          navigate("/");
+          setCount((count) => count + 1);
         }}
       >
-        Landing Page
+        Increase
       </button>
+
       <button
         onClick={() => {
-          navigate("/dashboard");
+          setCount((count) => count - 1);
         }}
       >
-        Dashboard Page
+        Decrease
       </button>
     </div>
   );
 }
+
+// function Routing() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <AppBar />
+//         <Routes>
+//           <Route
+//             path="/dashboard"
+//             element={
+//               <Suspense fallback={<h1>loading...</h1>}>
+//                 <Dashboard />
+//               </Suspense>
+//             }
+//           />
+//           <Route
+//             path="/"
+//             element={
+//               <Suspense fallback={<h1>loading...</h1>}>
+//                 <Landing />
+//               </Suspense>
+//             }
+//           />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// function AppBar() {
+//   const navigate = useNavigate();
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           navigate("/");
+//         }}
+//       >
+//         Landing Page
+//       </button>
+//       <button
+//         onClick={() => {
+//           navigate("/dashboard");
+//         }}
+//       >
+//         Dashboard Page
+//       </button>
+//     </div>
+//   );
+// }
 
 export default App;
