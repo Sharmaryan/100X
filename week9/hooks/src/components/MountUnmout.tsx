6ae33921df4f9eb4415a1814269bb93a284ddcf0
aryanoverdose <panditaryan.as@gmail.com>
2024-09-25 11:19:17 +0530

    class based comp

--- week9/hooks/src/components/MountUnmout.tsx
+++ week9/hooks/src/components/MountUnmout.tsx
@@ -8,18 +8,32 @@ export const MountUnmout = () => {
       setRender((prev) => !prev);
     }, 5000);
   }, []);
-
-  return <>{render ? <MyComponent /> : <div></div>}</>;
+  // return <>{render ? <MyComponent /> : <div></div>}</>;
+  return <>{render ? <ClassBasedExample /> : <div></div>}</>;
 };
 
-function MyComponent() {
-  useEffect(() => {
-    console.error("component mounted");
+// function MyComponent() {
+//   useEffect(() => {
+//     console.error("component mounted");
 
-    return () => {
-      console.log("component unmounted");
-    };
-  }, []);
+//     return () => {
+//       console.log("component unmounted");
+//     };
+//   }, []);
 
-  return <div>From inside my component</div>;
+//   return <div>From inside my component</div>;
+// }
+
+class ClassBasedExample extends React.Component {
+  componentDidMount() {
+    console.log('mounted')
+  }
+
+  componentWillUnmount() {
+    console.log('unmounted')
+  }
+
+  render() {
+    return <div>hey</div>
+  }
 }
