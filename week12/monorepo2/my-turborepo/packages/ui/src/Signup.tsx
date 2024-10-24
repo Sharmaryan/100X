f058dd24d5f1dc997391912064735bdd66371e85
aryanoverdose <panditaryan.as@gmail.com>
2024-10-24 20:58:20 +0530

    chore : added turborepo

---
+++ week12/monorepo2/my-turborepo/packages/ui/src/Signup.tsx
@@ -0,0 +1,15 @@
+export const Signup = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <input type="text" placeholder="text" />
+      <button>submit</button>
+    </div>
+  );
+};
