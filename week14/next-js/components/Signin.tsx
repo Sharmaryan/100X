00b413406c0c8ab93bc8a7c003558766a8a0a4a8
aryanoverdose <panditaryan.as@gmail.com>
2024-10-30 15:27:52 +0530

    fix : client component

--- week14/next-js/components/Signin.tsx
+++ week14/next-js/components/Signin.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link";
 
 export const SigninComponent = () => {
@@ -25,6 +26,9 @@ export const SigninComponent = () => {
               <button
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+                onClick={() => {
+                  alert("Signing in...");
+                }}
               >
                 Sign in
               </button>
