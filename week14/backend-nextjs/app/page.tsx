7988f5e15e502d3a08020d269e52151d9d76e2ed
aryanoverdose <panditaryan.as@gmail.com>
2024-10-30 16:49:39 +0530

    styles : card

--- week14/backend-nextjs/app/page.tsx
+++ week14/backend-nextjs/app/page.tsx
@@ -8,12 +8,16 @@ async function getUserDetails() {
 }
 
 export default async function Home() {
-  const userDetails = await getUserDetails();
+  const userData = await getUserDetails();
   return (
-    <div>
-      hi there
-      {userDetails.email}
-      {userDetails.name}
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData?.name}</div>
+
+          {userData?.email}
+        </div>
+      </div>
     </div>
   );
 }
