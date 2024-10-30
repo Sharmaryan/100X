6ee2fd1cec4ada5edb354c5b70f5f53ff535de03
aryanoverdose <panditaryan.as@gmail.com>
2024-10-30 15:35:06 +0530

    refactor : create button component(made it client)

--- week14/next-js/components/Signin.tsx
+++ week14/next-js/components/Signin.tsx
@@ -1,5 +1,5 @@
-'use client'
 import Link from "next/link";
+import { Button } from "./Button";
 
 export const SigninComponent = () => {
   return (
@@ -23,15 +23,7 @@ export const SigninComponent = () => {
                 type={"password"}
                 placeholder="123456"
               />
-              <button
-                type="button"
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-                onClick={() => {
-                  alert("Signing in...");
-                }}
-              >
-                Sign in
-              </button>
+              <Button />
             </div>
           </div>
         </Link>
