79cc6655a5d809f93fac13b1d40cfc9e62343033
aryanoverdose <panditaryan.as@gmail.com>
2024-11-14 16:34:41 +0530

    feat : layout route groups

---
+++ week14/nextjs-new/app/(auth)/(with-auth-layout)/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <>
+        <h2>Inner Layout</h2>
+        {children}
+      </>
+    );
+  }
+  
