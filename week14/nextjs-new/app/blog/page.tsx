6b3af651c15bedc1ad3358add62ea949072ab54a
aryanoverdose <panditaryan.as@gmail.com>
2024-11-14 16:59:45 +0530

    feat : meta data advance

--- week14/nextjs-new/app/blog/page.tsx
+++ week14/nextjs-new/app/blog/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: { absolute: "Blog" },
+};
 
 const Blog = () => {
-  return (
-    <h1>Blog page</h1>
-  )
-}
+  return <h1>Blog page</h1>;
+};
 
-export default Blog
+export default Blog;
