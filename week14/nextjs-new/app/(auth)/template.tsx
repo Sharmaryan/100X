ccdaae333e54b80097262680eb67141d403ecc06
aryanoverdose <panditaryan.as@gmail.com>
2024-11-15 20:06:47 +0530

    chore : replaced layout with template

--- week14/nextjs-new/app/(auth)/template.tsx
+++ week14/nextjs-new/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -14,6 +15,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
+  const [input, setInput] = useState("");
   return (
     <div
       style={{
@@ -22,9 +24,26 @@ export default function AuthLayout({
         gap: "10px",
       }}
     >
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        />
+      </div>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
-        return <Link key={link.href} href={link.href} className={isActive ? "text-blue-500": "text-black-500"}>{link.name}</Link>;
+        return (
+          <Link
+            key={link.href}
+            href={link.href}
+            className={isActive ? "text-blue-500" : "text-black-500"}
+          >
+            {link.name}
+          </Link>
+        );
       })}
       {children}
     </div>
