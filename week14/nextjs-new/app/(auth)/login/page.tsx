56907027023e87180ffb1eda8b16ffd44027b4bc
aryanoverdose <panditaryan.as@gmail.com>
2024-11-21 18:38:17 +0530

    feat : added login image

--- week14/nextjs-new/app/(auth)/login/page.tsx
+++ week14/nextjs-new/app/(auth)/login/page.tsx
@@ -1,8 +1,13 @@
+import Image from 'next/image'
 import React from 'react'
+import loginImage from '../../../public/Log_in.png'
 
 const Login = () => {
   return (
+    <div>
+    <Image src={loginImage} alt="login page"/>
     <h1>Login</h1>
+    </div>
   )
 }
 
