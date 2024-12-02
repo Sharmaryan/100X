b4ff06e97aac7629663bc8134a524284fc770d2d
aryanoverdose <panditaryan.as@gmail.com>
2024-12-02 16:11:19 +0530

    feat : unmatched routes

--- week14/nextjs-new/app/complex-dashboard/@notifications/page.tsx
+++ week14/nextjs-new/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
 import Card from "../../components/card";
 
 export default function Notifications(){
-    return <Card>Notifications</Card>
+    return <Card>
+        <div>Notifications</div>
+        <Link href={'/complex-dashboard/archived'} className="text-blue-700">Archived</Link>
+    </Card>
 }
