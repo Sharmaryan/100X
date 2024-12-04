a097c44e43bb8073c4ba9a74b888fee6d6f6f013
aryanoverdose <panditaryan.as@gmail.com>
2024-12-04 21:07:11 +0530

    feat : headers

--- week14/nextjs-new/app/profile/api/route.ts
+++ week14/nextjs-new/app/profile/api/route.ts
@@ -1,3 +1,14 @@
-export async function GET(){
-    return new Response("Profile API data");
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers)
+    const headersList = await headers()
+    console.log(requestHeaders.get('Authorization'));
+    console.log(headersList.get('Authorization'));
+    return new Response("<h1>Profile API data</h1>", {
+        headers:{
+            "Content-Type": "text/html"
+        }
+    });
 }
