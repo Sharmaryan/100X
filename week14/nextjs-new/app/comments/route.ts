14822c6776bf649ada2eef0dccd3f7ac30edd39a
aryanoverdose <panditaryan.as@gmail.com>
2024-12-04 19:29:12 +0530

    feat: post request

--- week14/nextjs-new/app/comments/route.ts
+++ week14/nextjs-new/app/comments/route.ts
@@ -1,6 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { comments } from "./data";
 
 export async function GET(){
+    return NextResponse.json(comments)
+}
+
+export async function POST(request: NextRequest){
+    const comment = await request.json()
+    const newComment = {
+        id: comments.length + 2,
+        text: comment.text
+    }
+    comments.push(newComment)
     return NextResponse.json(comments)
 }
