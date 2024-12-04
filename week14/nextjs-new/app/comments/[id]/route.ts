0bdbdec08cd36222d4bdc1e4bd122b0b59ab4652
aryanoverdose <panditaryan.as@gmail.com>
2024-12-04 20:56:33 +0530

    feat : redirects

--- week14/nextjs-new/app/comments/[id]/route.ts
+++ week14/nextjs-new/app/comments/[id]/route.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { comments } from "../data";
+import { redirect } from "next/navigation";
 
 export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
     const { id } = await params
     const comment = comments.find(item => item.id === parseInt(id))
+    if(parseInt(id) > comments.length){
+        redirect('/comments')
+    }
     if (!comment)
         return NextResponse.json({ msg: 'comment not found' })
     return NextResponse.json(comment)
