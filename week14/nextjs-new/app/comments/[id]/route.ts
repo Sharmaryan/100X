92e22a17e8e4d8fb1a5f40988dbb3cae4b17f466
aryanoverdose <panditaryan.as@gmail.com>
2024-12-04 19:48:13 +0530

    feat : patch request

--- week14/nextjs-new/app/comments/[id]/route.ts
+++ week14/nextjs-new/app/comments/[id]/route.ts
@@ -2,9 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { comments } from "../data";
 
 export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
-    const {id} = await params
+    const { id } = await params
     const comment = comments.find(item => item.id === parseInt(id))
-    if(!comment)
-    return NextResponse.json({msg:'comment not found'})
+    if (!comment)
+        return NextResponse.json({ msg: 'comment not found' })
     return NextResponse.json(comment)
+}
+
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = await params
+    const body = await request.json()
+    const { text } = body
+    console.log(id)
+    console.log(text)
+    const index = comments.findIndex(item => item.id === parseInt(id))
+    comments[index].text = text
+    return NextResponse.json(comments[index])
 }
