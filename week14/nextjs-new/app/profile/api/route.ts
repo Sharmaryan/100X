63c777afd9968e75199c8f5fcb1798d567f48f97
aryanoverdose <panditaryan.as@gmail.com>
2024-12-04 21:18:36 +0530

    feat : cookies

--- week14/nextjs-new/app/profile/api/route.ts
+++ week14/nextjs-new/app/profile/api/route.ts
@@ -1,14 +1,20 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers)
     const headersList = await headers()
+    const theme = request.cookies.get('theme')
+    ;(await cookies()).set('resultsPerPage','20')
+    console.log((await cookies()).get('resultsPerPage'))
+    console.log(theme)
     console.log(requestHeaders.get('Authorization'));
     console.log(headersList.get('Authorization'));
+
     return new Response("<h1>Profile API data</h1>", {
-        headers:{
-            "Content-Type": "text/html"
+        headers: {
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     });
 }
