e80841bd63002ef17b7fcb8c800e2670c1efa543
aryanoverdose <panditaryan.as@gmail.com>
2024-12-06 18:52:11 +0530

    chore : gen new component

--- week12/monorepo2/my-turborepo/apps/docs/app/admin/page.tsx
+++ week12/monorepo2/my-turborepo/apps/docs/app/admin/page.tsx
@@ -1,9 +1,10 @@
 import { Admin } from '@repo/ui/admin'
+import { InputBox } from '@repo/ui/input-box'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>docs:<Admin/></div>
+    <div>docs:<Admin/> <InputBox/></div>
   )
 }
 
